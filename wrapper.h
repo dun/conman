561bdb18fb124d37cf3dcb8f802cc377947a0794
Chris Dunlap <cdunlap@llnl.gov>
2001-09-25 23:17:12 +0000

    Added USE_PTHREADS ifdef.

--- wrapper.h
+++ wrapper.h
@@ -1,5 +1,5 @@
 /******************************************************************************\
- *  $Id: wrapper.h,v 1.1 2001/09/16 23:45:05 dun Exp $
+ *  $Id: wrapper.h,v 1.2 2001/09/25 23:17:12 dun Exp $
  *    by Chris Dunlap <cdunlap@llnl.gov>
 \******************************************************************************/
 
@@ -16,35 +16,39 @@
 #include "errors.h"
 
 
-#define x_pthread_mutex_init(MUTEX,ATTR)                                       \
+#ifdef USE_PTHREADS
+
+#  define x_pthread_mutex_init(MUTEX,ATTR)                                     \
      do {                                                                      \
          if ((errno = pthread_mutex_init((MUTEX), (ATTR))) != 0)               \
              err_msg(errno, "pthread_mutex_init() failed");                    \
      } while (0)
 
-#define x_pthread_mutex_lock(MUTEX)                                            \
+#  define x_pthread_mutex_lock(MUTEX)                                          \
      do {                                                                      \
          if ((errno = pthread_mutex_lock(MUTEX)) != 0)                         \
              err_msg(errno, "pthread_mutex_lock() failed");                    \
      } while (0)
 
-#define x_pthread_mutex_unlock(MUTEX)                                          \
+#  define x_pthread_mutex_unlock(MUTEX)                                        \
      do {                                                                      \
          if ((errno = pthread_mutex_unlock(MUTEX)) != 0)                       \
              err_msg(errno, "pthread_mutex_unlock() failed");                  \
      } while (0)
 
-#define x_pthread_mutex_destroy(MUTEX)                                         \
+#  define x_pthread_mutex_destroy(MUTEX)                                       \
      do {                                                                      \
          if ((errno = pthread_mutex_destroy(MUTEX)) != 0)                      \
              err_msg(errno, "pthread_mutex_destroy() failed");                 \
      } while (0)
 
-#define x_pthread_detach(THREAD)                                               \
+#  define x_pthread_detach(THREAD)                                             \
      do {                                                                      \
          if ((errno = pthread_detach(THREAD)) != 0)                            \
              err_msg(errno, "pthread_detach() failed");                        \
      } while (0)
+
+#endif /* USE_PTHREADS */
 
 
 #endif /* !_WRAPPER_H */
