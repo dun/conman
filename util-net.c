96ad39129f08427cd4f53c23b7a7b7127cbf95dc
Chris Dunlap <cdunlap@llnl.gov>
2001-12-20 20:11:13 +0000

    Fixed copy_hostent() word-alignment problems.

--- util-net.c
+++ util-net.c
@@ -1,5 +1,5 @@
 /******************************************************************************\
- *  $Id: util-net.c,v 1.9 2001/12/14 07:43:04 dun Exp $
+ *  $Id: util-net.c,v 1.10 2001/12/20 20:11:13 dun Exp $
  *    by Chris Dunlap <cdunlap@llnl.gov>
  ******************************************************************************
  *  Refer to "util-net.h" for documentation on public functions.
@@ -173,66 +173,50 @@ char * host_name_to_cname(const char *src, char *dst, int dstlen)
 }
 
 
-static int copy_hostent(const struct hostent *src, char *dst, int len)
+static int copy_hostent(const struct hostent *src, char *buf, int len)
 {
 /*  Copies the (src) hostent struct (and all of its associated data)
- *    into the buffer (dst) of length (len).
+ *    into the buffer (buf) of length (len).
  *  Returns 0 if the copy is successful, or -1 if the length of the buffer
  *    is not large enough to hold the result.
+ *
+ *  Note that the order in which data is copied into (buf) is done
+ *    in such a way as to ensure everything is properly word-aligned.
+ *    There is a method to the madness.
  */
-    struct hostent *hptr;
-    char *buf;
+    struct hostent *dst;
     int n;
     char **p, **q;
 
     assert(src != NULL);
-    assert(dst != NULL);
+    assert(buf != NULL);
 
-    hptr = (struct hostent *) dst;
-    buf = dst + sizeof(struct hostent);
+    dst = (struct hostent *) buf;
     if ((len -= sizeof(struct hostent)) < 0)
         return(-1);
-    hptr->h_addrtype = src->h_addrtype;
-    hptr->h_length = src->h_length;
-
-    /*  Copy h_name string.
-     */
-    hptr->h_name = buf;
-    n = strlcpy(buf, src->h_name, len);
-    buf += n + 1;			/* allow for trailing NUL char */
-    if ((len -= n + 1) < 0)
-        return(-1);
+    dst->h_addrtype = src->h_addrtype;
+    dst->h_length = src->h_length;
+    buf += sizeof(struct hostent);
 
     /*  Reserve space for h_aliases[].
      */
-    hptr->h_aliases = (char **) buf;
-    for (p=src->h_aliases, q=hptr->h_aliases; *p; p++, q++) {;}
-    buf = (char *) (q + 1);		/* allow for terminating NULL */
-    if ((len -= buf - (char *) hptr->h_aliases) < 0)
+    dst->h_aliases = (char **) buf;
+    for (p=src->h_aliases, q=dst->h_aliases, n=0; *p; p++, q++, n++) {;}
+    if ((len -= ++n * sizeof(char *)) < 0)
         return(-1);
-
-    /*  Copy h_aliases[] strings.
-     */
-    for (p=src->h_aliases, q=hptr->h_aliases; *p; p++, q++) {
-        n = strlcpy(buf, *p, len);
-        *q = buf;
-        buf += n + 1;			/* allow for trailing NUL char */
-        if ((len -= n + 1) < 0)
-            return(-1);
-    }
-    *q = NULL;
+    buf = (char *) (q + 1);
 
     /*  Reserve space for h_addr_list[].
      */
-    hptr->h_addr_list = (char **) buf;
-    for (p=src->h_addr_list, q=hptr->h_addr_list; *p; p++, q++) {;}
-    buf = (char *) (q + 1);		/* allow for terminating NULL */
-    if ((len -= buf - (char *) hptr->h_addr_list) < 0)
+    dst->h_addr_list = (char **) buf;
+    for (p=src->h_addr_list, q=dst->h_addr_list, n=0; *p; p++, q++, n++) {;}
+    if ((len -= ++n * sizeof(char *)) < 0)
         return(-1);
+    buf = (char *) (q + 1);
 
     /*  Copy h_addr_list[] in_addr structs.
      */
-    for (p=src->h_addr_list, q=hptr->h_addr_list; *p; p++, q++) {
+    for (p=src->h_addr_list, q=dst->h_addr_list; *p; p++, q++) {
         if ((len -= src->h_length) < 0)
             return(-1);
         memcpy(buf, *p, src->h_length);
@@ -241,7 +225,26 @@ static int copy_hostent(const struct hostent *src, char *dst, int len)
     }
     *q = NULL;
 
-    assert(verify_hostent(src, (struct hostent *) dst) >= 0);
+    /*  Copy h_aliases[] strings.
+     */
+    for (p=src->h_aliases, q=dst->h_aliases; *p; p++, q++) {
+        n = strlcpy(buf, *p, len);
+        *q = buf;
+        buf += ++n;			/* allow for trailing NUL char */
+        if ((len -= n) < 0)
+            return(-1);
+    }
+    *q = NULL;
+
+    /*  Copy h_name string.
+     */
+    dst->h_name = buf;
+    n = strlcpy(buf, src->h_name, len);
+    buf += ++n;				/* allow for trailing NUL char */
+    if ((len -= n) < 0)
+        return(-1);
+
+    assert(verify_hostent(src, dst) >= 0);
     return(0);
 }
 
