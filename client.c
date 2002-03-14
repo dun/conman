62a2015c1cb85f03c3e1ceb66d4291be1fb110be
Chris Dunlap <cdunlap@llnl.gov>
2002-03-14 03:37:00 +0000

    Added GPL copyleft and LLNL disclaimer.

--- client.c
+++ client.c
@@ -1,6 +1,34 @@
 /*****************************************************************************\
- *  $Id: client.c,v 1.13 2002/02/08 18:12:25 dun Exp $
- *    by Chris Dunlap <cdunlap@llnl.gov>
+ *  $Id: client.c,v 1.14 2002/03/14 03:37:00 dun Exp $
+ *****************************************************************************
+ *  Copyright (C) 2001-2002 The Regents of the University of California.
+ *  Written by Chris Dunlap <cdunlap@llnl.gov>.
+ *  UCRL-CODE-2002-009.
+ *  
+ *  This file is part of ConMan, a remote console management program.
+ *  For details, see <http://www.llnl.gov/linux/conman.html>.
+ *  
+ *  ConMan was produced at the University of California, Lawrence Livermore
+ *  National Laboratory (UC LLNL) under contract no. W-7405-ENG-48
+ *  (Contract 48) between the U.S. Department of Energy (DOE) and The Regents
+ *  of the University of California (University) for the operation of UC LLNL.
+ *  The rights of the Federal Government are reserved under Contract 48
+ *  subject to the restrictions agreed upon by the DOE and University as
+ *  allowed under DOE Acquisition Letter 97-1.
+ *  
+ *  ConMan is free software; you can redistribute it and/or modify it under
+ *  the terms of the GNU General Public License as published by the Free
+ *  Software Foundation; either version 2 of the License, or (at your option)
+ *  any later version.
+ *  
+ *  ConMan is distributed in the hope that it will be useful, but WITHOUT ANY
+ *  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ *  FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ *  details.
+ *  
+ *  You should have received a copy of the GNU General Public License along
+ *  with ConMan; if not, write to the Free Software Foundation, Inc.,
+ *  59 Temple Place, Suite 330, Boston, MA  02111-1307  USA.
 \*****************************************************************************/
 
 
