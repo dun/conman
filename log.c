3afb9e70f6555a4515d3c353976825587c43d42e
Chris Dunlap <cdunlap@llnl.gov>
2006-03-29 00:56:15 +0000

    * log.c: Updated copyright date.

--- log.c
+++ log.c
@@ -1,7 +1,7 @@
 /*****************************************************************************\
  *  $Id$
  *****************************************************************************
- *  Copyright (C) 2001-2002 The Regents of the University of California.
+ *  Copyright (C) 2001-2006 The Regents of the University of California.
  *  Produced at Lawrence Livermore National Laboratory (cf, DISCLAIMER).
  *  Written by Chris Dunlap <cdunlap@llnl.gov>.
  *  UCRL-CODE-2002-009.
